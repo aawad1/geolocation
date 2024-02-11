1344beb1f23a0f7a7e5cc92454a87a0e0d42f4b7
aawad1 <aawad1@etf.unsa.ba>
2024-02-11 16:08:33 +0100

    added buttons to switch from coordinates and place name, crated new view for coordinates

--- main/forms.py
+++ main/forms.py
@@ -9,3 +9,24 @@ class GeolocatorStatsForm(forms.ModelForm): #indicira da se radi o formi kreiran
         super(GeolocatorStatsForm, self).__init__(*args, **kwargs)
         self.fields['query'].widget.attrs.update({"class": "form-control", "placeholder": "Enter geocoding query"})
     
+class  GeolocatorStatsFormLL(forms.ModelForm):
+    class Meta:
+        model=GeolocatorStats
+        fields=["latitude", "longitude"]
+        
+    def __init__(self, *args, **kwargs):
+        super(GeolocatorStatsFormLL, self).__init__(*args, **kwargs)
+        self.fields['latitude'].widget.attrs.update({"class": "form-control", "placeholder": "Enter latitude"})
+        self.fields['longitude'].widget.attrs.update({"class": "form-control", "placeholder": "Enter longitude"})    
+    
+    def clean_latitude(self):
+        latitude = self.cleaned_data['latitude']
+        if latitude < -90 or latitude > 90:
+            raise forms.ValidationError("Latitude must be between -90 and 90 degrees.")
+        return latitude
+
+    def clean_longitude(self):
+        longitude = self.cleaned_data['longitude']
+        if longitude < -180 or longitude > 180:
+            raise forms.ValidationError("Longitude must be between -180 and 180 degrees.")
+        return longitude
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import GeolocatorStatsForm
+from .forms import GeolocatorStatsFormLL
 from .models import GeolocatorStats
 import requests
 
@@ -26,6 +27,36 @@ def forward_geocode(address, format='json'):
     }
 
     response = requests.get(base_url, params=params)
+    data = response.json()
+
+    return data
+######################################
+def coordinates(request):
+    # Your view logic goes here
+    
+    if request.method == "POST":
+        form = GeolocatorStatsFormLL(request.POST)
+        if form.is_valid():
+            latitude = form.cleaned_data['latitude']
+            longitude = form.cleaned_data['longitude']
+            form.save()
+            return render(request, 'coordinates.html', {"form": form, "place_name": reverse_geocode(latitude, longitude)}) 
+    else:
+         form = GeolocatorStatsFormLL()   
+    
+    context = {"form": form}        
+    return render(request, "coordinates.html", context=context)
+
+def reverse_geocode(latitude, longitude, format='json'):
+    base_url = 'https://nominatim.openstreetmap.org/reverse'
+    params = {
+        'lat': latitude,
+        'lon': longitude,
+        'format': format,
+    }
+
+    response = requests.get(base_url, params=params)
+    print(response.content)
     data = response.json()
 
     return data
