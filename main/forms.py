b43a994a879d3f4c5bd4ec2065acf856872dcfc3
aawad1 <aawad1@etf.unsa.ba>
2024-02-19 16:01:20 +0100

    did navbar, history, about, and parsing query

--- main/forms.py
+++ main/forms.py
@@ -4,29 +4,15 @@ from .models import GeolocatorStats
 class GeolocatorStatsForm(forms.ModelForm): #indicira da se radi o formi kreiranoj na osnovu modela
     class Meta:
         model=GeolocatorStats
-        fields=["query"]
+        fields=["query", "latitude", "longitude"]
     def __init__(self, *args, **kwargs):
         super(GeolocatorStatsForm, self).__init__(*args, **kwargs)
         self.fields['query'].widget.attrs.update({"class": "form-control", "placeholder": "Enter geocoding query"})
     
-class  GeolocatorStatsFormLL(forms.ModelForm):
+class  GeolocatorStatsFormHistory(forms.ModelForm):
     class Meta:
         model=GeolocatorStats
-        fields=["latitude", "longitude"]
-        
+        fields=["query", "latitude", "longitude"]
     def __init__(self, *args, **kwargs):
-        super(GeolocatorStatsFormLL, self).__init__(*args, **kwargs)
-        self.fields['latitude'].widget.attrs.update({"class": "form-control", "placeholder": "Enter latitude"})
-        self.fields['longitude'].widget.attrs.update({"class": "form-control", "placeholder": "Enter longitude"})    
-    
-    def clean_latitude(self):
-        latitude = self.cleaned_data['latitude']
-        if latitude < -90 or latitude > 90:
-            raise forms.ValidationError("Latitude must be between -90 and 90 degrees.")
-        return latitude
-
-    def clean_longitude(self):
-        longitude = self.cleaned_data['longitude']
-        if longitude < -180 or longitude > 180:
-            raise forms.ValidationError("Longitude must be between -180 and 180 degrees.")
-        return longitude
+        super(GeolocatorStatsForm, self).__init__(*args, **kwargs)
+        self.fields['query'].widget.attrs.update({"class": "form-control", "placeholder": "Enter geocoding query"})
--- main/views.py
+++ main/views.py
@@ -1,24 +1,77 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import GeolocatorStatsForm
-from .forms import GeolocatorStatsFormLL
+from .forms import GeolocatorStatsFormHistory
 from .models import GeolocatorStats
 import requests
+import re
 
+# HOW TO DEAL WITH ERROR MESSAGES 
+history = []
+#navbar, history, about, home
 def index(request):
     resp = ""
     if request.method == "POST":
         form = GeolocatorStatsForm(request.POST)
+        print(form)
         if form.is_valid():
             resp = form.cleaned_data['query']
-            form.save()
-            return render(request, 'locator.html', {"form": form, "queries": forward_geocode(resp)}) 
+            result = validate_input(resp)
+            print(result)
+            #provjeriti unos da li je koordinate ili naziv, mozda koristiti regex
+            if(result==None):
+                context = {"form": form}  
+                return render(request,'locator.html', context=context)
+            if(len(result) == 1):
+                print(resp)
+                fwg = forward_geocode(resp)
+                if(not fwg):
+                    context = {"form": form}  
+                    return render(request,'locator.html', context=context)
+                else:
+                    form.instance.query = fwg[0].get('name')
+                    form.instance.latitude = fwg[0].get('latitude')
+                    form.instance.longitude = fwg[0].get('longitude')
+                    print(form)
+                    #history.append(fwg[0])
+
+                    form.save()
+                    return render(request, 'locator.html', {"form": form, "queries": fwg[0]})
+            elif(len(result) == 2):
+                [lat, lon] = result
+                rvg = reverse_geocode(lat, lon)
+                print(rvg)
+                form.save()
+                return render(request, 'locator.html', {"form": form, "queries": rvg})
+                
     else:
          form = GeolocatorStatsForm()   
     
     context = {"form": form}        
+    #stats = GeolocatorStats.objects.all() #object related mapper (ORM)
+    #history = stats
+    #context['stats'] = stats
+    #print(stats)
     return render(request, 'locator.html', context=context)
 
+def validate_input(input_str):
+    # Split the input into two numbers
+    parts = re.split(r',', input_str)
+
+    try:
+        if(len(parts) > 1):
+            # Convert the parts to floats
+            latitude, longitude = map(float, parts)
+            return [latitude,longitude]
+            # Check if latitude and longitude are within the valid range
+            if -90 <= latitude <= 90 and -180 <= longitude <= 180:
+                return [latitude, longitude]
+            else:
+                return [200,200]
+        else:
+            return [input_str]
+    except ValueError:
+        return None
 def forward_geocode(address, format='json'):
     base_url = 'https://nominatim.openstreetmap.org/search'
     params = {
@@ -27,8 +80,9 @@ def forward_geocode(address, format='json'):
     }
 
     response = requests.get(base_url, params=params)
+    
     data = response.json()
-
+    print(data)
     return data
 ######################################
 def coordinates(request):
@@ -40,7 +94,7 @@ def coordinates(request):
             latitude = form.cleaned_data['latitude']
             longitude = form.cleaned_data['longitude']
             form.save()
-            return render(request, 'coordinates.html', {"form": form, "place_name": reverse_geocode(latitude, longitude)}) 
+            return render(request, 'coordinates.html', {"form": form, "place_name": reverse_geocode(latitude, longitude)[0]}) 
     else:
          form = GeolocatorStatsFormLL()   
     
@@ -58,5 +112,14 @@ def reverse_geocode(latitude, longitude, format='json'):
     response = requests.get(base_url, params=params)
     print(response.content)
     data = response.json()
-
+    print(data)
     return data
+
+##############################
+def about(request):
+    return render(request, "about.html")
+
+##############################
+def history(request):
+    history = GeolocatorStats.objects.all()
+    return render(request, "history.html", {'history': history})
